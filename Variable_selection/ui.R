4435abeb96292895471e3ab2b2c9c152b97f7cf0
L_Sokol <36013697+2lsokol2@users.noreply.github.com>
2019-03-11 18:09:40 +0200

    Update ui.R

--- Variable_selection/ui.R
+++ Variable_selection/ui.R
@@ -1,4 +1,5 @@
 library(shiny) # load the shiny package
+
 work_data <- readRDS(file = "work_data.rds")
 
 # Define UI for application
@@ -13,11 +14,11 @@ shinyUI(fluidPage(
     
     selectInput("var", label = "1. Select Variable", 
                 choices = names(work_data)[c(2, 4 , 5 , 7 , 8, 10, 11 ,13 ,15 ,16 ,18, 20, 21)],
-                selected = 'chk_ac_status_1', multiple = T),
+                selected = 'chk_ac_status_1'),
     
     selectInput("dataset", label = "2. Select dataset", 
                 choices = c("Train" = 1, "Train1" = 2, "Train2" = 3, "Test"=4),
-                selected = 1, multiple = T)
+                selected = 1)
     
   ),
   
@@ -28,8 +29,5 @@ shinyUI(fluidPage(
                 tabPanel("Plot", plotOutput("plot")),
                 tabPanel("Summary",verbatimTextOutput("sum")),
                 tabPanel("Structure", verbatimTextOutput("str"))
-  
-))))
-
-
-
+                
+    ))))
